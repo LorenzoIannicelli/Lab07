d27188b915279f50002d41a0462fce3a8e448551
s341841 <s341841@studenti.polito.it>
2025-11-12 14:28:19 +0100

    PROGRAMMA FINITO

--- model/model.py
+++ model/model.py
@@ -20,18 +20,18 @@ class Model:
     def get_artefatti_filtrati(self, museo:str, epoca:str):
         """Restituisce la lista di tutti gli artefatti filtrati per museo e/o epoca (filtri opzionali)."""
 
-        '''if museo == 'Nessun filtro' and epoca == 'Nessun filtro' :
+        if museo == 'NULL' and epoca == 'NULL' :
             return self._lista_artefatti
-        elif museo == 'Nessun filtro'  :
+        elif museo == 'NULL'  :
             return [artefatto for artefatto in self._lista_artefatti if artefatto.epoca == epoca]
-        elif epoca == 'Nessun filtro' :
+        elif epoca == 'NULL' :
             return [artefatto for artefatto in self._lista_artefatti if artefatto.id_museo == museo]
         else :
-            return [artefatto for artefatto in self._lista_artefatti if artefatto.id_museo == museo and artefatto.epoca == epoca]'''
+            return [artefatto for artefatto in self._lista_artefatti if artefatto.id_museo == museo and artefatto.epoca == epoca]
 
-        self._lista_artefatti_filtrati = self._artefatto_dao.read_artefatti_filtrati(museo, epoca)
+        '''self._lista_artefatti_filtrati = self._artefatto_dao.read_artefatti_filtrati(museo, epoca)
 
-        return self._lista_artefatti_filtrati
+        return self._lista_artefatti_filtrati'''
 
     def get_epoche(self):
         """Restituisce la lista di tutte le epoche."""
